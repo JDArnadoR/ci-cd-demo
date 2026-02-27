3fee13a0d73e553a83699a8661ca9fec0406a5d1
Julio Arnado <20252d074@khipu.edu.pe>
2026-02-27 18:18:32 -0500

    fix: tests corregidos

--- tests/test_crud.py
+++ tests/test_crud.py
@@ -1,15 +1,18 @@
 import crud
 
 def test_create_and_read():
+    crud.data.clear()
     crud.create("item1")
     assert "item1" in crud.read()
 
 def test_update():
+    crud.data.clear()
     crud.create("item2")
     assert crud.update(0, "item2_updated")
     assert crud.read()[0] == "item2_updated"
 
 def test_delete():
+    crud.data.clear()
     crud.create("item3")
     assert crud.delete(0)
     assert "item3" not in crud.read()
